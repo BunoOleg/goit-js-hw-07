e7f317451b7341eecef62edb85f5260ddd874e6e
BunoOleg <86609721+BunoOleg@users.noreply.github.com>
2021-11-20 07:33:00 +0200

    дз7

---
+++ js/02-lightbox.js
@@ -0,0 +1,28 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const gallery = document.querySelector(".gallery");
+
+const galleryPic = galleryItems
+  .map(
+    (galleryItem) =>
+      `<a class="gallery__item" href="${galleryItem.original}">
+  <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+</a>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("afterbegin", galleryPic);
+
+const openOriginal = function (event) {
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionType: "attr",
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+  });
+
+  event.preventDefault();
+};
+
+gallery.addEventListener("click", openOriginal);
