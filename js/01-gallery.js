6ce7c5564745abe215deec7bd31e83fd35ebec0a
BunoOleg <86609721+BunoOleg@users.noreply.github.com>
2022-05-02 17:17:10 +0300

    HW 7 is done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,27 +1,30 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const gallery = document.querySelector(".gallery");
+const divRef = document.querySelector(".gallery");
 
-const galleryPic = galleryItems
-  .map(
-    (galleryItem, index) =>
-      `<div class="gallery__item">
-  <a class="gallery__link" href="${galleryItem.original}">
-    <img
-      class="gallery__image"
-      src="${galleryItem.preview}"
-      data-source="${galleryItem.original}"
-      data-index="${index}"
-      alt="${galleryItem.description}"
-    />
-  </a>
-</div>`
-  ).join("");
+const markupRender = (items) => {
+  return items
+    .map(
+      (item) => `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+            <img
+                class="gallery__image"
+                src="${item.preview}"
+                data-source="${item.original}"
+                alt="${item.description}"
+            />
+        </a>
+    </div>
+    `
+    )
+    .join("");
+};
 
-let currentIndex = 0;
+const markup = markupRender(galleryItems);
 
-gallery.insertAdjacentHTML("afterbegin", galleryPic);
+divRef.innerHTML = markup;
 
 const instance = basicLightbox.create(
   `<img src="" alt="" width="800" height="600"/>`,
@@ -35,37 +38,24 @@ const instance = basicLightbox.create(
   }
 );
 
-function setModalSrc(url) {
-  instance.element().querySelector("img").src = url;
-}
-
-const onEscClose = function (event) {
-  if (event.code === "Escape") {
+const onEscClose = (e) => {
+  if (e.code === "Escape") {
     instance.close();
   }
-  if (event.code === "ArrowLeft") {
-    currentIndex -= 1;
-    if (currentIndex <= 0) {
-      currentIndex = galleryItems.length - 1;
-    }
-    setModalSrc(galleryItems[currentIndex].original);
-  }
-  if (event.code === "ArrowRight") {
-    currentIndex += 1;
-    if (currentIndex >= galleryItems.length - 1) {
-      currentIndex = 0;
-    }
-    setModalSrc(galleryItems[currentIndex].original);
-  }
 };
-const openOriginal = function (event) {
-  event.preventDefault();
-  if (event.target.nodeName !== "IMG") return;
 
-  currentIndex = Number(event.target.dataset.index);
-  console.log(currentIndex);
+const setModalSrc = (url) =>
+  (instance.element().querySelector("img").src = url);
 
-  setModalSrc(event.target.dataset.source);
+const onModalOpen = (e) => {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const targetImg = e.target.dataset.source;
+  setModalSrc(targetImg);
   instance.show();
 };
-gallery.addEventListener("click", openOriginal);
+
+divRef.addEventListener("click", onModalOpen);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,28 +1,27 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const gallery = document.querySelector(".gallery");
+const divRef = document.querySelector(".gallery");
 
-const galleryPic = galleryItems
-  .map(
-    (galleryItem) =>
-      `<a class="gallery__item" href="${galleryItem.original}">
-  <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
-</a>`
-  )
-  .join("");
-
-gallery.insertAdjacentHTML("afterbegin", galleryPic);
-
-const openOriginal = function (event) {
-  const lightbox = new SimpleLightbox(".gallery a", {
-    captionType: "attr",
-    captionsData: "alt",
-    captionPosition: "bottom",
-    captionDelay: 250,
-  });
-
-  event.preventDefault();
+const markupRender = (items) => {
+  return items
+    .map(
+      (item) => `
+    
+  <a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>
+`
+    )
+    .join("");
 };
 
-gallery.addEventListener("click", openOriginal);
+const markup = markupRender(galleryItems);
+
+divRef.innerHTML = markup;
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
